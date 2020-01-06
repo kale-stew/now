deb4a8a42939bcde1c776459165db31f9b75c40f
Kylie Stewart <kylieastewart@gmail.com>
2020-01-05 17:31:59 -0700

    finally got the footer to stick

--- src/App.js
+++ src/App.js
@@ -7,43 +7,50 @@ function App() {
 
   return (
     <div>
-      <header />
-      <h1>What I'm Doing Now</h1>
-      <h3>{mostRecent.date}</h3>
-      <div className="summary">
-        <ul className="now">
-          <li>
-            <b>Location:</b> {mostRecent.location}
-          </li>
-          <li>
-            <b>Travel:</b> {mostRecent.travel}
-          </li>
-          <li>
-            <b>Reading:</b> {mostRecent.reading}
-          </li>
-          <li>
-            <b>Celebrating:</b> {mostRecent.celebrating}
-          </li>
-          <li>
-            <b>Watching:</b> {mostRecent.watching}
-          </li>
-          <li>
-            <b>Listening to:</b> {mostRecent.listeningTo}
-          </li>
-          <li>
-            <b>Learning:</b> {mostRecent.learning}
-          </li>
-          <li>
-            <b>Working On:</b> {mostRecent.workingOn}
-          </li>
-        </ul>
-      </div>
-      <br />
-      <br />
-      <a href="https://nownownow.com/about">What is this page?</a>
-      <br />
-      <br />
-      <footer />
+      <body>
+        <header />
+        <div className="content">
+          <h1>What I'm Doing Now</h1>
+          <h3>{mostRecent.date}</h3>
+          <div className="inner-content">
+            <ul className="now">
+              <li>
+                <b>Location:</b> {mostRecent.location}
+              </li>
+              <li>
+                <b>Travel:</b> {mostRecent.travel}
+              </li>
+              <li>
+                <b>Reading:</b> {mostRecent.reading}
+              </li>
+              <li>
+                <b>Celebrating:</b> {mostRecent.celebrating}
+              </li>
+              <li>
+                <b>Watching:</b> {mostRecent.watching}
+              </li>
+              <li>
+                <b>Listening to:</b> {mostRecent.listeningTo}
+              </li>
+              <li>
+                <b>Learning:</b> {mostRecent.learning}
+              </li>
+              <li>
+                <b>Working On:</b> {mostRecent.workingOn}
+              </li>
+            </ul>
+          </div>
+          <br />
+          <br />
+          <a href="https://nownownow.com/about">What is this page?</a>
+          <br />
+          <br />
+          <a href="https://kylieis.online/now">
+            Visit a random "now" from the past
+          </a>
+        </div>
+        <footer />
+      </body>
     </div>
   );
 }
