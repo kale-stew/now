8a464870e1051cee2a00e3e195f8c36eb1f5e416
Kylie Stewart <kylie@kyliestewart.tech>
2020-04-10 14:23:52 -0600

    improvements

--- src/index.js
+++ src/index.js
@@ -1,58 +1,59 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Footer from './components/footer';
+import { formatDate, getMostRecentDate } from './utils';
 import './index.css';
 
-import * as posts from './now.json';
-const mostRecent = posts[0];
+import posts from './now.json';
+
+const mostRecentDate = getMostRecentDate(posts);
+const mostRecentPost = posts[mostRecentDate];
 
 const App = () => (
-  <Fragment style={{ height: '100%' }}>
-    <body>
-      <header />
-      <div className="content">
-        <h1>What I'm Doing Now</h1>
-        <h3>{mostRecent.date}</h3>
-        <div className="inner-content">
-          <ul className="now">
-            <li>
-              <b>Location:</b> {mostRecent.location}
-            </li>
-            <li>
-              <b>Travel:</b> {mostRecent.travel}
-            </li>
-            <li>
-              <b>Reading:</b> {mostRecent.reading}
-            </li>
-            <li>
-              <b>Celebrating:</b> {mostRecent.celebrating}
-            </li>
-            <li>
-              <b>Watching:</b> {mostRecent.watching}
-            </li>
-            <li>
-              <b>Listening to:</b> {mostRecent.listeningTo}
-            </li>
-            <li>
-              <b>Learning:</b> {mostRecent.learning}
-            </li>
-            <li>
-              <b>Working On:</b> {mostRecent.workingOn}
-            </li>
-          </ul>
-        </div>
-        <br />
-        <br />
-        <a href="https://nownownow.com/about">What is this page?</a>
-        <br />
-        <br />
-        <a href="https://kylieis.online/now">
-          Visit a random "now" from the past
-        </a>
+  <body>
+    <header />
+    <div className="content">
+      <h1>What I'm Doing Now</h1>
+      <h3>{formatDate(mostRecentDate)}</h3>
+      <div className="inner-content">
+        <ul className="now">
+          <li>
+            <b>Location:</b> {mostRecentPost.location}
+          </li>
+          <li>
+            <b>Travel:</b> {mostRecentPost.travel}
+          </li>
+          <li>
+            <b>Reading:</b> {mostRecentPost.reading}
+          </li>
+          <li>
+            <b>Celebrating:</b> {mostRecentPost.celebrating}
+          </li>
+          <li>
+            <b>Watching:</b> {mostRecentPost.watching}
+          </li>
+          <li>
+            <b>Listening to:</b> {mostRecentPost.listeningTo}
+          </li>
+          <li>
+            <b>Learning:</b> {mostRecentPost.learning}
+          </li>
+          <li>
+            <b>Working On:</b> {mostRecentPost.workingOn}
+          </li>
+        </ul>
       </div>
-      <Footer />
-    </body>
-  </Fragment>
+      <br />
+      <br />
+      <a href="https://nownownow.com/about">What is this page?</a>
+      <br />
+      <br />
+      <a href="https://kylieis.online/now">
+        Visit a random "now" from the past
+      </a>
+    </div>
+    <Footer />
+  </body>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
