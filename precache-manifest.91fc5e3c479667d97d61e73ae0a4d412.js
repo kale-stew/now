3d30c2d73d2b45b15634a23f173b50898a54977a
Kylie Stewart <kylie@kyliestewart.tech>
2020-07-19 17:44:28 -0600

    Updates

---
+++ precache-manifest.91fc5e3c479667d97d61e73ae0a4d412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce6022a217890d0f9ca5e75fe3eb98d",
+    "url": "/now/index.html"
+  },
+  {
+    "revision": "147fcb646c8a9eab2437",
+    "url": "/now/static/css/main.46cee075.chunk.css"
+  },
+  {
+    "revision": "ee11bc6038f01474c36b",
+    "url": "/now/static/js/2.ac4868a4.chunk.js"
+  },
+  {
+    "revision": "147fcb646c8a9eab2437",
+    "url": "/now/static/js/main.f25161e2.chunk.js"
+  },
+  {
+    "revision": "79985369e3181d437a69",
+    "url": "/now/static/js/runtime-main.35a28251.js"
+  }
+]);
